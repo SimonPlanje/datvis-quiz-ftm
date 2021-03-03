01386970bd3ff85fe58ddc37e64bd72dbfcd2d83
Richard Jong <richard@firstbranch.nl>
2021-03-03 10:34:18 +0100

    Added link to endscreen

--- src/components/Check.js
+++ src/components/Check.js
@@ -101,7 +101,7 @@ function handleEnd(){
     </div>  
     <div className="checkBtns">
     <button className='prevBtn' onClick={handlePrevious}>Vorige</button>
-    {showBtn ? <button className='C-nextBtn'  onClick={handleEnd}>Afronden</button>
+    {showBtn ? <a href='https://www.ftm.nl/gids-partijfinanciering' className='btn C-nextBtn'>Afronden</a>
              : <button className='C-nextBtn' onClick={handleNext}>Volgende</button>
     }
     </div>    
