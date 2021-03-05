4c2fe135818eabb03a1d4d62a6675114b8c3b25f
Simon <simon.planje2000@gmail.com>
2021-03-05 14:30:31 +0100

    added welcome/loading screen

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -6,7 +6,7 @@ export default function Loading() {
 
   return (
     <div className="Loading">
-        <RotateLoader />
+        <RotateLoader className='loader' />
         <img src={ftmLogo} alt='logo Follow the Money'></img>
       </div>
   );
