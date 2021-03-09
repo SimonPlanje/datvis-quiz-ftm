48ad9394dec908f6240d323dad443440d03d50f3
Richard Jong <richard@firstbranch.nl>
2021-03-09 20:09:51 +0100

    end > start

--- src/App.js
+++ src/App.js
@@ -213,7 +213,7 @@ export default function App() {
         setTotData
       );
       // Data is loaded, set initial view
-      setView('end');
+      setView('start');
     }
     getData();
   }, []);
