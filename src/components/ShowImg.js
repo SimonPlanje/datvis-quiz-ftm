575961a40344116569c60a3c84afd9e479a4e764
Simon <simon.planje2000@gmail.com>
2021-03-09 16:37:05 +0100

    removed type of question above img

--- src/components/ShowImg.js
+++ src/components/ShowImg.js
@@ -1,7 +1,6 @@
 export default function ShowImg({ quiz, currentQuestion }) {
   return (
     <div className='ShowImg'>
-      <p>{quiz[currentQuestion].type}</p>
       <div className='imgPart'>
         <img src={quiz[currentQuestion].image} alt='afbeelding bij vraag' />
       </div>
