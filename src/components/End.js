8f785d02f3671242e471051437f3f69ae25c9927
Richard Jong <richard@firstbranch.nl>
2021-03-09 21:10:46 +0100

    fixed onclicks and sharetext

--- src/components/End.js
+++ src/components/End.js
@@ -50,10 +50,21 @@ class End extends Component {
     if (localStorage.getItem('antwoorden')) {
       const getAns = JSON.parse(localStorage.getItem('antwoorden'));
 
-      let count = 0;
+      let count = 1;
       getAns.forEach((e) => (e.check === true ? count++ : ''));
       this.setState({ correctCount: count });
     }
+  }
+
+  windowOpener(e) {
+    e.preventDefault();
+    const url = e.target.closest('a').getAttribute('href');
+    window.open(
+      url,
+      '',
+      'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600'
+    );
+    return false;
   }
 
   render(countCorrect, setCountCorrect) {
@@ -69,45 +80,42 @@ class End extends Component {
             <div className='socialIcons'>
               <a
                 className='facebook'
-                href='https://www.facebook.com/dialog/share?app_id=1641639456124709&amp;display=popup&amp;href=https%3A%2F%2Fwww.ftm.nl%2Fpartijfinanciering-quiz%26utm_medium%3Dsocial%26utm_campaign%3Dsharebuttonleden%26utm_source%3Dfacebook&amp;redirect_uri=https%3A%2F%2Fftm.local%2Fapp_dev.php%2Fartikelen%2Frabobank-investeert-miljarden-in-amerikaanse-vleesgigant-tyson%3Futm_medium%3Dsocial%26utm_campaign%3Dsharebuttonleden%26utm_source%3Dfacebook'
-                onclick="javascript:window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');return false;"
+                href='https://www.facebook.com/dialog/share?app_id=1641639456124709&amp;display=popup&amp;href=https%3A%2F%2Fwww.ftm.nl%2Fpartijfinanciering-quiz%3Futm_medium%3Dsocial%26utm_campaign%3Dsharebuttonleden%26utm_source%3Dfacebook&amp;redirect_uri=https%3A%2F%2Fwww.ftm.nl%2Fpartijfinanciering-quiz%3Futm_medium%3Dsocial%26utm_campaign%3Dsharebuttonleden%26utm_source%3Dfacebook'
+                onClick={this.windowOpener}
               >
                 <i className='fa fa-facebook'></i>
               </a>
               <a
                 className='twitter'
-                href='https://twitter.com/intent/tweet?text=&amp;url=https%3A%2F%2Fwww.ftm.nl%2Fpartijfinanciering-quiz%26utm_medium%3Dsocial%26utm_campaign%3Dsharebuttonleden%26utm_source%3Dtwitter&amp;via=ftm_nl'
-                onclick="javascript:window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');return false;"
+                href={
+                  'https://twitter.com/intent/tweet?text=Ik%20had%20' +
+                  this.state.correctCount +
+                  '%20vragen%20goed%20van%20de%20politieke%20advertenties%20quiz.%20Doe%20ook%20mee!&amp;url=https%3A%2F%2Fwww.ftm.nl%2Fpartijfinanciering-quiz%3Futm_medium%3Dsocial%26utm_campaign%3Dsharebuttonleden%26utm_source%3Dtwitter&amp;via=ftm_nl'
+                }
+                onClick={this.windowOpener}
               >
                 <i className='fa fa-twitter'></i>
               </a>
               <a
                 className='linkedin'
-                href='https://www.linkedin.com/shareArticle?url=https%3A%2F%2Fwww.ftm.nl%2Fpartijfinanciering-quiz%26utm_medium%3Dsocial%26utm_campaign%3Dsharebuttonleden%26utm_source%3Dlinkedin&amp;title=&amp;source=Follow%20The%20Money'
-                onclick="javascript:window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');return false;"
+                href='https://www.linkedin.com/shareArticle?url=https%3A%2F%2Fwww.ftm.nl%2Fpartijfinanciering-quiz%3Futm_medium%3Dsocial%26utm_campaign%3Dsharebuttonleden%26utm_source%3Dlinkedin&amp;source=Follow%20The%20Money'
+                onClick={this.windowOpener}
               >
                 <i className='fa fa-linkedin'></i>
               </a>
               <a
                 className='mail'
-                href='mailto:?to=&amp;body=https%3A%2F%2Fwww.ftm.nl%2Fpartijfinanciering-quiz%26utm_medium%3Dsocial%26utm_campaign%3Dsharebuttonleden%26utm_source%3Dmail &amp;subject='
+                href='mailto:?to=&amp;body=https%3A%2F%2Fwww.ftm.nl%2Fpartijfinanciering-quiz%3Futm_medium%3Dsocial%26utm_campaign%3Dsharebuttonleden%26utm_source%3Dmail &amp;subject='
                 target='_blank'
               >
                 <i className='fa fa-envelope'></i>
               </a>
               <a
                 className='whatsapp'
-                href='whatsapp://send?text=%20https%3A%2F%2Fwww.ftm.nl%2Fpartijfinanciering-quiz%26utm_medium%3Dsocial%26utm_campaign%3Dsharebuttonleden%26utm_source%3Dwhatsapp'
+                href='whatsapp://send?text=%20https%3A%2F%2Fwww.ftm.nl%2Fpartijfinanciering-quiz%3Futm_medium%3Dsocial%26utm_campaign%3Dsharebuttonleden%26utm_source%3Dwhatsapp'
                 target='_blank'
               >
                 <i className='fa fa-whatsapp'></i>
-              </a>
-              <a
-                className='link'
-                href='https://www.ftm.nl/partijfinanciering-quiz%26utm_medium=social&amp;utm_campaign=sharebuttonleden&amp;utm_source=linkbutton'
-                onclick='javascript:return false;'
-              >
-                <i className='fa fa-link'></i>
               </a>
             </div>
           </div>
