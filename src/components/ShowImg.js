068aa786fc8d9c0a00763dcb794baa801061c522
Simon <simon.planje2000@gmail.com>
2021-03-09 15:13:01 +0100

    fixed checkbox snapping by images

--- src/components/ShowImg.js
+++ src/components/ShowImg.js
@@ -1,10 +1,10 @@
-export default function ShowImg({quiz, currentQuestion}) {
-
-
+export default function ShowImg({ quiz, currentQuestion }) {
   return (
-    <div className="ShowImg">
-        <p>{quiz[currentQuestion].type}</p>
+    <div className='ShowImg'>
+      <p>{quiz[currentQuestion].type}</p>
+      <div className='imgPart'>
         <img src={quiz[currentQuestion].image} alt='afbeelding bij vraag' />
+      </div>
     </div>
   );
 }
